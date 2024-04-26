32fc47a41ff2455de439674ceca0c668ceac6af5
Nurenaissance <154663519+Nurenaissance@users.noreply.github.com>
2024-04-26 12:32:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,9 +11,7 @@ import {getdata} from './fromFirestore.js';
 
 const WEBHOOK_VERIFY_TOKEN = "COOL";
 
-const GRAPH_API_TOKEN = "EAAVZBobCt7AcBO8trGDsP8t4bTe2mRA7sNdZCQ346G9ZANwsi4CVdKM5MwYwaPlirOHAcpDQ63LoHxPfx81tN9h2SUIHc1LUeEByCzS8eQGH2J7wwe9tqAxZAdwr4SxkXGku2l7imqWY16qemnlOBrjYH3dMjN4gamsTikIROudOL3ScvBzwkuShhth0rR9P";
 
-const GPT_API_KEY = "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";
 
 const PORT = 8080;
 

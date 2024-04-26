0bda6da556ddf1c6b569d80fd9435dbb1c823c82
Nurenaissance <154663519+Nurenaissance@users.noreply.github.com>
2024-04-26 17:26:33 +0530

    Update send_whatsapp_msg.js

--- send_whatsapp_msg.js
+++ send_whatsapp_msg.js
@@ -3,9 +3,9 @@ import axios from "axios";
 import {addConversation, conversationData} from "./server.js";
 const WEBHOOK_VERIFY_TOKEN = "COOL";
 
-const GRAPH_API_TOKEN = "EAAVZBobCt7AcBO63vGk4bL6OBfrZCNZC39XlB43ZCrSqZCKwUlt3WEpvlOEeS5vvsYrSaiD6Wi1ikVmGntaHQstF77vJZCDbyaByy5vl6ZCiluB9nmwGW3SvkaW5OM702jIqKRRLybWReJhpAV4sZA8l96EZCokVNXTgcZAYj0iMcu82X2VxNkm77up1YPkUPi6K0kBJsE5Xi5r7srXKloMiAZD";
+const GRAPH_API_TOKEN = process.env.GRAPH_API_TOKEN;
 
-const GPT_API_KEY = "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";
+const GPT_API_KEY = process.env.REACT_APP_OPENAI_KEY;
 
 async function sendWhatsappMessage(phoneNumber, message) {
   const url = "https://graph.facebook.com/v18.0/241683569037594/messages";

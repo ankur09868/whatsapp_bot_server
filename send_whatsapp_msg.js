5ecf5f08cb20bed5a26e106182b5c9f909c99898
Shreyas Vashistha <138184214+Shreyas9548@users.noreply.github.com>
2024-04-30 18:42:37 +0530

    Add files via upload

--- send_whatsapp_msg.js
+++ send_whatsapp_msg.js
@@ -27,7 +27,7 @@ async function sendWhatsappMessage(phoneNumber, message) {
     },
   };
   addConversation(phoneNumber, message, ".", undefined);
-  console.log(conversationData);
+  //console.log(conversationData);
 
   axios.post(url, data, config).then((response) => {
        console.log("Response:", response.data);

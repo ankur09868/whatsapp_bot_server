6fda245191b20eda7116a734faaf936e8f19ea90
Shreyas9548 <shreyasv828@gmail.com>
2024-08-27 11:48:17 +0530

    27.08.24.1

--- send_whatsapp_msg.js
+++ send_whatsapp_msg.js
@@ -1,6 +1,5 @@
 
 import axios from "axios";
-import {addConversation, conversationData} from "./server.js";
 const WEBHOOK_VERIFY_TOKEN = "COOL";
 
 const GRAPH_API_TOKEN="EAAVZBobCt7AcBO8trGDsP8t4bTe2mRA7sNdZCQ346G9ZANwsi4CVdKM5MwYwaPlirOHAcpDQ63LoHxPfx81tN9h2SUIHc1LUeEByCzS8eQGH2J7wwe9tqAxZAdwr4SxkXGku2l7imqWY16qemnlOBrjYH3dMjN4gamsTikIROudOL3ScvBzwkuShhth0rR9P";

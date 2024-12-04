7714a08922ae969daa12f36a141995ce18b9bdae
Shreyas9548 <shreyasv828@gmail.com>
2024-12-04 21:22:34 +0530

    added start script

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Start both server and worker with PM2
+
+# Start the server.js script
+pm2 start server.js --name "server"
+
+# Start the worker.js script
+pm2 start worker.js --name "worker"
+
+# Save PM2 process list
+pm2 save
+
+# Start PM2 in daemon mode
+pm2 startup
+
+# Keep the shell script running (to prevent the container from exiting)
+tail -f /dev/null

662f8235bbbc49f49ce14c0932561c174a48e778
Shreyas9548 <shreyasv828@gmail.com>
2024-12-04 17:56:53 +0530

    evert "worker.js"
    This reverts commit 235dac965f9ac43406abab6a0fa5c85e112c4a92.

--- queues/messageQueue.js
+++ queues/messageQueue.js
@@ -1,13 +1,11 @@
 import Queue from 'bull';
-import dotenv from 'dotenv';
 
-dotenv.config()
 // Initialize the queue
 export const messageQueue = new Queue('messageQueue', {
   redis: { 
     host: 'whatsappnuren.redis.cache.windows.net', 
     port: 6379 ,
-    password: process.env.REDIS_PASSWORD
+    password: 'O6qxsVvcWHfbwdgBxb1yEDfLeBv5VBmaUAzCaJvnELM='
 }, // Configure Redis connection
 });
 
